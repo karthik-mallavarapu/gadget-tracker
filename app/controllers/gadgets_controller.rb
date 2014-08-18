0c141ef5087759e7bdea8c9532b93508f7592320
Karthik Mallavarapu <karthik.mallavarapu@gmail.com>
2014-08-18 13:07:03 +0300

    added gadgets as a nested resource

---
+++ app/controllers/gadgets_controller.rb
@@ -0,0 +1,53 @@
+class GadgetsController < ApplicationController
+
+  before_filter :authenticate_user!
+  before_filter :find_gadget, only: [:show, :edit, :destroy]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @gadget = current_user.gadgets.build
+  end
+
+  def create
+    @gadget = current_user.gadgets.build(gadget_params)
+    if @gadget.save
+      redirect_to [current_user, @gadget], flash: {success: "A new Gadget has been added."}
+    else
+      flash[:alert] = "Gadget has not been added."
+      render action: "new"
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def gadget_params
+    params.require(:gadget).permit(:name, :description)
+  end
+
+  def find_gadget
+    @gadget = current_user.gadgets.find(params[:id])  
+    rescue
+      flash[:alert] = "The gadget you were looking for could not be found."
+      redirect_to user_gadgets_path(current_user)
+  end
+
+end
